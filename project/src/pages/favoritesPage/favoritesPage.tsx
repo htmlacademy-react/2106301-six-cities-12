0c328cc9d3d36e8b54dbf71bc7a748726a2b5dc1
Capsonchik <Serega00790@mail.ru>
2023-03-05 12:45:21 +0300

    Пофикшены ошибки линтера

--- project/src/pages/favoritesPage/favoritesPage.tsx
+++ project/src/pages/favoritesPage/favoritesPage.tsx
@@ -1,4 +1,4 @@
-export const FavoritesPage = () => {
+export function FavoritesPage() {
   return (
     <div className="page">
       <header className="header">
@@ -65,8 +65,7 @@ export const FavoritesPage = () => {
                           <b className="place-card__price-value">&euro;180</b>
                           <span className="place-card__price-text">&#47;&nbsp;night</span>
                         </div>
-                        <button className="place-card__bookmark-button place-card__bookmark-button--active button"
-                                type="button">
+                        <button className="place-card__bookmark-button place-card__bookmark-button--active button" type="button">
                           <svg className="place-card__bookmark-icon" width="18" height="19">
                             <use xlinkHref="#icon-bookmark"></use>
                           </svg>
@@ -103,8 +102,7 @@ export const FavoritesPage = () => {
                           <b className="place-card__price-value">&euro;80</b>
                           <span className="place-card__price-text">&#47;&nbsp;night</span>
                         </div>
-                        <button className="place-card__bookmark-button place-card__bookmark-button--active button"
-                                type="button">
+                        <button className="place-card__bookmark-button place-card__bookmark-button--active button" type="button">
                           <svg className="place-card__bookmark-icon" width="18" height="19">
                             <use xlinkHref="#icon-bookmark"></use>
                           </svg>
@@ -153,8 +151,7 @@ export const FavoritesPage = () => {
                           <b className="place-card__price-value">&euro;180</b>
                           <span className="place-card__price-text">&#47;&nbsp;night</span>
                         </div>
-                        <button className="place-card__bookmark-button place-card__bookmark-button--active button"
-                                type="button">
+                        <button className="place-card__bookmark-button place-card__bookmark-button--active button" type="button">
                           <svg className="place-card__bookmark-icon" width="18" height="19">
                             <use xlinkHref="#icon-bookmark"></use>
                           </svg>
--- project/src/pages/offerPage/offerPage.tsx
+++ project/src/pages/offerPage/offerPage.tsx
@@ -1,6 +1,6 @@
-import {Header} from "../../components/header/header";
+import {Header} from '../../components/header/header';
 
-export const OfferPage = () => {
+export function OfferPage() {
   return (
     <div className="page">
       <Header/>
@@ -105,8 +105,7 @@ export const OfferPage = () => {
                 <h2 className="property__host-title">Meet the host</h2>
                 <div className="property__host-user user">
                   <div className="property__avatar-wrapper property__avatar-wrapper--pro user__avatar-wrapper">
-                    <img className="property__avatar user__avatar" src="/img/avatar-angelina.jpg" width="74" height="74"
-                         alt="Host avatar"/>
+                    <img className="property__avatar user__avatar" src="/img/avatar-angelina.jpg" width="74" height="74" alt="Host avatar"/>
                   </div>
                   <span className="property__user-name">
                     Angelina
@@ -132,8 +131,7 @@ export const OfferPage = () => {
                   <li className="reviews__item">
                     <div className="reviews__user user">
                       <div className="reviews__avatar-wrapper user__avatar-wrapper">
-                        <img className="reviews__avatar user__avatar" src="/img/avatar-max.jpg" width="54" height="54"
-                             alt="Reviews avatar"/>
+                        <img className="reviews__avatar user__avatar" src="/img/avatar-max.jpg" width="54" height="54" alt="Reviews avatar"/>
                       </div>
                       <span className="reviews__user-name">
                         Max
@@ -157,16 +155,14 @@ export const OfferPage = () => {
                 <form className="reviews__form form" action="#" method="post">
                   <label className="reviews__label form__label" htmlFor="review">Your review</label>
                   <div className="reviews__rating-form form__rating">
-                    <input className="form__rating-input visually-hidden" name="rating" value="5" id="5-stars"
-                           type="radio"/>
+                    <input className="form__rating-input visually-hidden" name="rating" value="5" id="5-stars" type="radio"/>
                     <label htmlFor="5-stars" className="reviews__rating-label form__rating-label" title="perfect">
                       <svg className="form__star-image" width="37" height="33">
                         <use xlinkHref="#icon-star"></use>
                       </svg>
                     </label>
 
-                    <input className="form__rating-input visually-hidden" name="rating" value="4" id="4-stars"
-                           type="radio"/>
+                    <input className="form__rating-input visually-hidden" name="rating" value="4" id="4-stars" type="radio"/>
                     <label htmlFor="4-stars" className="reviews__rating-label form__rating-label" title="good">
                       <svg className="form__star-image" width="37" height="33">
                         <use xlinkHref="#icon-star"></use>
@@ -174,35 +170,34 @@ export const OfferPage = () => {
                     </label>
 
                     <input className="form__rating-input visually-hidden" name="rating" value="3" id="3-stars" type="radio"/>
-                      <label htmlFor="3-stars" className="reviews__rating-label form__rating-label" title="not bad">
-                        <svg className="form__star-image" width="37" height="33">
-                          <use xlinkHref="#icon-star"></use>
-                        </svg>
-                      </label>
+                    <label htmlFor="3-stars" className="reviews__rating-label form__rating-label" title="not bad">
+                      <svg className="form__star-image" width="37" height="33">
+                        <use xlinkHref="#icon-star"></use>
+                      </svg>
+                    </label>
 
-                      <input className="form__rating-input visually-hidden" name="rating" value="2" id="2-stars" type="radio"/>
-                        <label htmlFor="2-stars" className="reviews__rating-label form__rating-label" title="badly">
-                          <svg className="form__star-image" width="37" height="33">
-                            <use xlinkHref="#icon-star"></use>
-                          </svg>
-                        </label>
+                    <input className="form__rating-input visually-hidden" name="rating" value="2" id="2-stars" type="radio"/>
+                    <label htmlFor="2-stars" className="reviews__rating-label form__rating-label" title="badly">
+                      <svg className="form__star-image" width="37" height="33">
+                        <use xlinkHref="#icon-star"></use>
+                      </svg>
+                    </label>
 
-                        <input className="form__rating-input visually-hidden" name="rating" value="1" id="1-star" type="radio"/>
-                          <label htmlFor="1-star" className="reviews__rating-label form__rating-label"
-                                 title="terribly">
-                            <svg className="form__star-image" width="37" height="33">
-                              <use xlinkHref="#icon-star"></use>
-                            </svg>
-                          </label>
+                    <input className="form__rating-input visually-hidden" name="rating" value="1" id="1-star" type="radio"/>
+                    <label htmlFor="1-star" className="reviews__rating-label form__rating-label" title="terribly">
+                      <svg className="form__star-image" width="37" height="33">
+                        <use xlinkHref="#icon-star"></use>
+                      </svg>
+                    </label>
                   </div>
-                  <textarea className="reviews__textarea form__textarea" id="review" name="review"
-                            placeholder="Tell how was your stay, what you like and what can be improved"></textarea>
+                  <textarea className="reviews__textarea form__textarea" id="review" name="review" placeholder="Tell how was your stay, what you like and what can be improved"></textarea>
                   <div className="reviews__button-wrapper">
                     <p className="reviews__help">
                       To submit review please make sure to set <span className="reviews__star">rating</span> and
                       describe your stay with at least <b className="reviews__text-amount">50 characters</b>.
                     </p>
-                    <button className="reviews__submit form__submit button" type="submit" disabled={undefined}>Submit</button>
+                    <button className="reviews__submit form__submit button" type="submit" disabled={undefined}>Submit
+                    </button>
                   </div>
                 </form>
               </section>
@@ -226,8 +221,7 @@ export const OfferPage = () => {
                       <b className="place-card__price-value">&euro;80</b>
                       <span className="place-card__price-text">&#47;&nbsp;night</span>
                     </div>
-                    <button className="place-card__bookmark-button place-card__bookmark-button--active button"
-                            type="button">
+                    <button className="place-card__bookmark-button place-card__bookmark-button--active button" type="button">
                       <svg className="place-card__bookmark-icon" width="18" height="19">
                         <use xlinkHref="#icon-bookmark"></use>
                       </svg>
@@ -318,5 +312,5 @@ export const OfferPage = () => {
         </div>
       </main>
     </div>
-);
-};
+  );
+}

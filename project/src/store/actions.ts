6889284c3d8ff8269dc2b1bc5d33c1262ee970c9
Capsonchik <Serega00790@mail.ru>
2023-03-28 17:43:30 +0300

    Добавленны экшены и редьюсер

---
+++ project/src/store/actions.ts
@@ -0,0 +1,4 @@
+import {createAction} from "@reduxjs/toolkit";
+
+export const addCity = createAction('addCity')
+export const addOffers = createAction('addOffers')
---
+++ project/src/store/reduser.ts
@@ -0,0 +1,24 @@
+import {createReducer} from "@reduxjs/toolkit";
+import {addCity, addOffers} from "./actions";
+import {offers} from "../mocks/offers";
+import {Offers} from "../types/offer";
+
+const emptyOfferList: Offers = []
+
+const initialState = {
+  city: '',
+  offers: emptyOfferList
+}
+
+export const reduser = createReducer(initialState, (builder) => {
+  builder
+    .addCase(addCity, (state) => {
+      state.city = 'Paris'
+    })
+    .addCase(addOffers, (state) => {
+      state.offers = offers
+    })
+})
+
+
+
